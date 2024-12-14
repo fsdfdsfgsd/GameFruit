00759990ed5151ed8ddde5a1ba92094ad4c7099e
涛涛 <3200172527@qq.com>
2024-12-14 20:42:37 +0800

    碰撞后换场景

---
+++ parkour1203/Assets/Scripts/Finsih.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finsih : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            FindObjectOfType<AudioMusic>().playerPlay(3);
+            //Invoke("finsihLevel", 1f);
+            finsihLevel();
+        }
+    }
+
+    void finsihLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
